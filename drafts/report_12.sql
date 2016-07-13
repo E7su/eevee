3af25573940d0f833f2e635ff391a6eee5f685eb
E7su <etsu4296@gmail.com>
2016-07-13 11:48:11 +0300

    code optimization

--- drafts/report_12.sql
+++ drafts/report_12.sql
@@ -3,29 +3,32 @@
 -- Description:       A report on the number of bugs per release,
 --                    found in the component, integration testing,
 --                    and production environments for the selected period
---                    ​for selected command
+--                    for selected command
 
 --------------------------------------//12//---------------------------------------
 SELECT
-  tp.TEAM "Команда",
-  COUNT(j.ID)
+  tp.TEAM     "Команда",
+  COUNT(j.ID) "Баги",
+  CASE j.ISSUESTATUS
+  WHEN '11405'
+    THEN 'Component Test'
+  WHEN '11410'
+    THEN 'Integration Test'
+  WHEN '10015'
+    THEN 'Production'
+  WHEN '10088'
+    THEN 'Component Test'
+  ELSE ''
+  END         "Этап"
 FROM jira.JIRAISSUE j
   JOIN jira.PROJECT p ON j.PROJECT = p.ID
   JOIN V_REP_TEAMS_PROJECTS tp ON (tp.PROJECT = p.PNAME)
-WHERE j.ISSUESTATUS IN (10090, 11405, 11410, 10015) AND
-      -- [10090]  Интеграционное тестирование [Done]
+WHERE j.ISSUESTATUS IN (11405, 11410, 10015, 10088) AND
       -- [11405]  Component Test [Done]
       -- [11410]  Integration Test [Done]
       -- [10015]  Done
-      j.issuetype IN
+      -- [10088]  Компонентное тестирование [Done]
+      j.ISSUETYPE IN
       (57, 49, 10603, 10702, 1, 41, 55, 10601, 42)
-      -- [57]     Дефект
-      -- [49]     Bug Sub-task
-      -- [10603]  Дефект УС
-      -- [10702]  Bugtask
       -- [1]      Bug
-      -- [41]     Дефект мобильного приложения
-      -- [55]     Дефект для доски
-      -- [10601]  Дефект сервера
-      -- [42]     Дефект бэка
-GROUP BY tp.TEAM
+GROUP BY tp.TEAM, j.ISSUESTATUS
