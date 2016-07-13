024d1f275e0b2e0cd989208f5debb0f11e6328bf
E7su <etsu4296@gmail.com>
2016-07-13 11:13:00 +0300

    replace projects -> teams

--- drafts/report_9.sql
+++ drafts/report_9.sql
@@ -1,23 +1,25 @@
-﻿-- Date of creation:  05.05.2016
+-- Author:            Polina Azarova
+-- Date of creation:  05.05.2016
 -- Description:       Report (Graph ), the average duration of the task
 --                    in the context of the type ( User story / Bug / Task)
 --                    and the team for the period
 
 --------------------------------------//09//---------------------------------------
-
 SELECT
-  p.pname                     "Команда",
-  it.pname                    "Тип",
-  AVG(cg.created - j.created) "Длит-ть задачи"
+  tp.TEAM                               "Команда",
+  it.PNAME                              "Тип",
+  TRUNC(AVG(cg.CREATED - j.CREATED), 2) "Длит-ть задачи"
 FROM JIRA.JIRAISSUE j
-  JOIN jira.project p ON j.project = p.id
-  JOIN jira.issuetype it ON j.issuetype = it.id
-  JOIN jira.changegroup cg ON (cg.issueid = j.id)
-  JOIN jira.changeitem ci ON (ci.groupid = cg.id)
-WHERE it.pname IN ('User Story', 'Bug', 'Task')
-    --   AND TRUNC(j.created) >= TRUNC(to_date('$year_st-$month_st-$day_st', 'yyyy-mm-dd'))
-    --   AND TRUNC(cg.created) <= TRUNC(to_date('$year_end-$month_end-$day_end', 'yyyy-mm-dd'))
-    --   AND p.pname = '$team' AND it.pname = '$type'
-      AND ci.field = 'status' AND to_char(newstring) = 'Done'
-GROUP BY p.pname, it.pname
+  JOIN jira.PROJECT p ON j.PROJECT = p.ID
+  JOIN jira.ISSUETYPE it ON j.ISSUETYPE = it.ID
+  JOIN jira.CHANGEGROUP cg ON (cg.ISSUEID = j.ID)
+  JOIN jira.CHANGEITEM ci ON (ci.GROUPID = cg.ID)
+  JOIN V_REP_TEAMS_PROJECTS tp ON (tp.PROJECT = p.PNAME)
+WHERE it.PNAME IN ('User Story', 'Bug', 'Task')
+      AND TRUNC(j.CREATED) >= TRUNC(TO_DATE('$year_st-$month_st-01', 'yyyy-mm-dd'))
+      AND TRUNC(cg.CREATED) <= TRUNC(ADD_MONTHS(TO_DATE('$year_st-$month_st-01', 'yyyy-mm-dd'), 1))
+      AND ('$team' IS NULL OR tp.TEAM = '$team')
+      AND ('$type' IS NULL OR it.PNAME = '$type')
+      AND ci.FIELD = 'status' AND TO_CHAR(ci.NEWSTRING) = 'Done'
+GROUP BY tp.TEAM, it.PNAME
 ORDER BY 1
