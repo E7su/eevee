0a835ec7a0bc0d2a9fa0aea24d33e2e6664fcb66
E7su <etsu4296@gmail.com>
2016-07-13 10:29:29 +0300

    replace projects -> teams

--- drafts/report_11.sql
+++ drafts/report_11.sql
@@ -1,4 +1,5 @@
-﻿-- Date of creation:  05.05.2016
+-- Author:            Polina Azarova
+-- Date of creation:  05.05.2016
 -- Description:       Report (graph) in the number of implemented tasks 
 --                    for the selected period in the sections :
 --                    -commands and all;
@@ -6,23 +7,18 @@
 
 --------------------------------------------//11//---------------------------------------------
 SELECT
-  p.pname           "Команда",
-  vs.value          "Размер задачи",
-  count(j.issuenum) "Количество issue"
+  p.pname                               "Команда",
+  it.pname                              "Тип",
+  TRUNC(AVG(cg.created - j.created), 2) "Длит-ть задачи"
 FROM JIRA.JIRAISSUE j
   JOIN jira.project p ON j.project = p.id
   JOIN jira.issuetype it ON j.issuetype = it.id
   JOIN jira.changegroup cg ON (cg.issueid = j.id)
-  JOIN jira.customfieldvalue cfv ON j.id = cfv.issue
-  JOIN V_SIZES vs ON cfv.stringvalue = to_char(vs.id)
   JOIN jira.changeitem ci ON (ci.groupid = cg.id)
-WHERE ci.field = 'status' AND to_char(newstring) = 'Done'
-      AND TRUNC(j.created) >= TRUNC(to_date('2016-01-01', 'yyyy-mm-dd'))
-      --   AND TRUNC(j.created) >= TRUNC(to_date('$year_st-$month_st-$day_st', 'yyyy-mm-dd'))
-      --   AND TRUNC(cg.created) <= TRUNC(to_date('$year_end-$month_end-$day_end', 'yyyy-mm-dd'))
-      AND p.pname IS NOT NULL
-    --  OR p.pname = '$team'
-         AND it.pname IS NOT NULL
-    --  OR it.pname = '$size'
-GROUP BY p.pname, value
-ORDER BY 1, 2 DESC
+WHERE it.pname IN ('User Story', 'Bug', 'Task')
+      AND TRUNC(j.created) >= TRUNC(to_date('$year_st-$month_st-$day_st', 'yyyy-mm-dd'))
+      AND TRUNC(cg.created) <= TRUNC(to_date('$year_end-$month_end-$day_end', 'yyyy-mm-dd'))
+      AND p.pname = '$team' AND it.pname = '$type'
+      AND ci.field = 'status' AND to_char(newstring) = 'Done'
+GROUP BY p.pname, it.pname
+ORDER BY 1
