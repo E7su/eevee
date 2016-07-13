03694f1dc6e25ddb5d4f3ba9208a4aba81ad2402
E7su <etsu4296@gmail.com>
2016-07-13 13:09:19 +0300

    code refactoring

--- drafts/report_10.sql
+++ drafts/report_10.sql
@@ -1,6 +1,6 @@
 -- Author:            Polina Azarova
 -- Date of creation:  05.05.2016
--- Description:       Report (Graph ), the average duration of the task in the context 
+-- Description:       Report (Graph), the average duration of the task in the context 
 --                    of the size of the issue and the team for the period
 
 --------------------------------------//10//---------------------------------------
@@ -8,7 +8,7 @@ SELECT
   tp.TEAM                               "Команда",
   vs.VALUE                              "Размер задачи",
   TRUNC(AVG(cg.CREATED - j.CREATED), 2) "Длит-ть задачи"
-FROM JIRA.JIRAISSUE j
+FROM jira.JIRAISSUE j
   JOIN jira.PROJECT p ON j.PROJECT = p.ID
   JOIN jira.ISSUETYPE it ON j.ISSUETYPE = it.ID
   JOIN jira.CHANGEGROUP cg ON (cg.ISSUEID = j.ID)
