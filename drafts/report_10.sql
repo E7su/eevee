8a223baed0951d839e61232069dfcc3f1c83628a
E7su <etsu4296@gmail.com>
2016-07-15 11:13:23 +0300

    rename alias for eazyBI

--- drafts/report_10.sql
+++ drafts/report_10.sql
@@ -1,14 +1,15 @@
 -- Author:            Polina Azarova
 -- Date of creation:  05.05.2016
--- Description:       Report (Graph), the average duration of the task in the context 
+-- Description:       Report (Graph):
+--                    the average duration of the task in the context
 --                    of the size of the issue and the team for the period
 
 --------------------------------------//10//---------------------------------------
 SELECT
-  tp.TEAM                               "Команда",
-  vs.VALUE                              "Размер задачи",
-  TRUNC(AVG(cg.CREATED - j.CREATED), 2) "Длит-ть задачи"
-FROM jira.JIRAISSUE j
+  tp.TEAM,
+  vs.VALUE                              TASK_SIZE,
+  TRUNC(AVG(cg.CREATED - j.CREATED), 2) DURATION
+FROM JIRA.JIRAISSUE j
   JOIN jira.PROJECT p ON j.PROJECT = p.ID
   JOIN jira.ISSUETYPE it ON j.ISSUETYPE = it.ID
   JOIN jira.CHANGEGROUP cg ON (cg.ISSUEID = j.ID)
