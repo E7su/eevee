098022d10176c005c298f6d9dd5a8f27965aaf8f
E7su <etsu4296@gmail.com>
2016-07-22 11:22:48 +0300

    fix calculation (defects in production)

--- drafts/report_12.sql
+++ drafts/report_12.sql
@@ -1,36 +1,23 @@
 -- Author:            Polina Azarova
 -- Date of creation:  20.05.2016
 -- Description:       Report (Graph):
---                    the number of bugs per release,
---                    found in the component, integration testing,
---                    and production for the selected period
---                    for selected team
+--                    KPI 
+--                    the average quantity of defects found in production
 
 --------------------------------------//12//---------------------------------------
 SELECT
   tp.TEAM,
-  COUNT(j.ID) BUG,
-  CASE j.ISSUESTATUS
-  WHEN '11405'
-    THEN 'Component Test'
-  WHEN '11410'
-    THEN 'Integration Test'
-  WHEN '10015'
-    THEN 'Production'
-  WHEN '10088'
-    THEN 'Component Test'
-  ELSE ''
-  END         STAGE
+  -- количество багов, найденных в фазу тестирования Пром. эксплуатация
+  count(cf.CFNAME) QUANTITY
 FROM jira.JIRAISSUE j
   JOIN jira.PROJECT p ON j.PROJECT = p.ID
   JOIN V_REP_TEAMS_PROJECTS tp ON (tp.PROJECT = p.PNAME)
-WHERE j.ISSUESTATUS IN (11405, 11410, 10015, 10088) AND
-      -- [11405]  Component Test [Done]
-      -- [11410]  Integration Test [Done]
-      -- [10015]  Done
-      -- [10088]  Компонентное тестирование [Done]
-      j.ISSUETYPE IN
-      (57, 49, 10603, 10702, 1, 41, 55, 10601, 42)
+  JOIN jira.CUSTOMFIELDVALUE cfv ON cfv.ISSUE = j.ID
+  JOIN jira.CUSTOMFIELD cf ON cf.ID = cfv.CUSTOMFIELD
+  JOIN jira.CUSTOMFIELDOPTION cfo ON cfo.CUSTOMFIELD = cf.ID
+WHERE j.ISSUETYPE IN (1, 41) AND
       -- [1]      Bug
-      AND TRUNC(j.CREATED) >= TRUNC(TO_DATE('$year_st-$month_st-01', 'yyyy-mm-dd'))
-GROUP BY tp.TEAM, j.ISSUESTATUS
+      -- [41]     Дефект мобильного приложения
+      cfv.CUSTOMFIELD = '11573' AND cfo.ID = '11848' AND -- 'Фаза тестирования' 'Пром. эксплуатация'
+      TRUNC(j.CREATED) >= TRUNC(TO_DATE('2016-01-01', 'yyyy-mm-dd'))
+GROUP BY tp.TEAM
