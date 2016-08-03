c7b3fbe7412db0f128e406107e013807347f33ab
Etsu <etsu4296@gmail.com>
2016-08-03 17:55:41 +0300

    improve display for holiday in diff months
    Change date format
    Example: '3.10' -> '03.10'
    
    Improve data display for the situation with holiday in the different months
    Example: '25.04-06.05' -> '25.04-30.04 ' and '01.05-06.05'

--- drafts/x-men_headers_vacation.sql
+++ drafts/x-men_headers_vacation.sql
@@ -2,21 +2,23 @@
 -- Date of creation:  19.05.2016
 -- Description:       X-MEN HEADERS vacation
 
-CREATE OR REPLACE VIEW V_REP_X-MEN_HEADERS_HOLIDAY AS
+CREATE OR REPLACE VIEW V_REP_CITEB_HEADERS_HOLIDAY AS
   SELECT
     s.ФИО,
-    s."1" || e."1"   "Январь",
-    s."2" || e."2"   "Февраль",
-    s."3" || e."3"   "Март",
-    s."4" || e."4"   "Апрель",
-    s."5" || e."5"   "Май",
-    s."6" || e."6"   "Июнь",
-    s."7" || e."7"   "Июль",
-    s."8" || e."8"   "Август",
-    s."9" || e."9"   "Сентябрь",
-    s."10" || e."10" "Октябрь",
-    s."11" || e."11" "Ноябрь",
-    s."12" || e."12" "Декабрь",
+    -- обработка ситуации, когда отпуск попал на границу месяцев
+    -- конкат нужен для того, чтобы не потерять вторую часть отпуска (которая попала на соседний месяц)
+    s."1" || e."1"   Январь,
+    s."2" || e."2"   Февраль,
+    s."3" || e."3"   Март,
+    s."4" || e."4"   Апрель,
+    s."5" || e."5"   Май,
+    s."6" || e."6"   Июнь,
+    s."7" || e."7"   Июль,
+    s."8" || e."8"   Август,
+    s."9" || e."9"   Сентябрь,
+    s."10" || e."10" Октябрь,
+    s."11" || e."11" Ноябрь,
+    s."12" || e."12" Декабрь,
     s.Итого
   FROM
     (SELECT
@@ -24,39 +26,56 @@ CREATE OR REPLACE VIEW V_REP_X-MEN_HEADERS_HOLIDAY AS
        i.HOLIDAY Итого
      FROM
        (SELECT *
-        FROM (SELECT
-                tt.FIO                       ФИО,
-                tt.RN,
-                LISTAGG(tt.HOLIDAY, ', ')
-                WITHIN GROUP (
-                  ORDER BY tt.HOLIDAY_ORDER) HOLIDAY
-              FROM
-                (SELECT
-                   ROWNUM HOLIDAY_ORDER,
-                   t.FIO,
-                   t.RN,
-                   t.HOLIDAY
-                 FROM (SELECT
-                         vac.FIO,
-                         -- в ячейку месяца начала отпуска
-                         vac.RN_S RN,
-                         CASE vac.END_DAY
-                         WHEN vac.START_DAY
-                           THEN TO_CHAR(vac.END_DAY) -- удаление даты начала, если отпуск длиной один день
-                         ELSE vac.START_DAY || '.' || vac.START_MONTH || '-' || vac.END_DAY || '.' ||
-                              vac.END_MONTH -- через чёрточку, если дата начала != дате конца
-                         END      HOLIDAY
-                       FROM V_REP_CITEB_HEADERS_DATES vac
-                       ORDER BY vac.FIO, vac.RN_E, vac.START_DAY
-                      ) t
-                ) tt
-              GROUP BY tt.FIO, tt.RN)
-              PIVOT (MAX(HOLIDAY)
-                FOR RN
-                IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))) h
+        FROM
+          (SELECT
+             tt.FIO                       ФИО,
+             tt.RN,
+             -- через запятую в таблице
+             LISTAGG(tt.HOLIDAY, ', ')
+             WITHIN GROUP (
+               ORDER BY tt.HOLIDAY_ORDER) HOLIDAY
+           FROM
+             (SELECT
+                ROWNUM HOLIDAY_ORDER,
+                t.FIO,
+                t.RN,
+                t.HOLIDAY
+              FROM (SELECT
+                      vac.FIO,
+                      -- в ячейку месяца начала отпуска
+                      vac.RN_S RN,
+                      CASE vac.END_DAY
+                      WHEN vac.START_DAY
+                        THEN vac.END_DAY -- удаление даты начала, если отпуск длиной один день
+                      ELSE
+                        -- а если отпуск не один день, то
+                        (CASE vac.END_MONTH
+                         -- если отпуск не попал на границу месяцов,
+                         -- то есть дата начала отпуска совпадает с датой конца,
+                         -- выводим через чёрточку время отпуска
+                         WHEN vac.START_MONTH
+                           THEN
+                             vac.START_DAY || '.' || vac.START_MONTH || '-' || vac.END_DAY || '.' || vac.END_MONTH
+                         -- если попал на границу, то пишем что он был до последнего дня месяца
+                         -- и месяц конца будет как месяц начала
+                         -- а конец отпуска (который попал на другой месяц подтягивается из нижней части джойна)
+                         ELSE vac.START_DAY || '.' || vac.START_MONTH || '-' ||
+                              TO_CHAR(LAST_DAY(TO_DATE(vac.DATE_START, 'YYYY-MM-DD')), 'DD')
+                              || '.' || vac.START_MONTH
+                         END)
+                      END      HOLIDAY
+                    FROM V_REP_CITEB_HEADERS_DATES vac
+                    ORDER BY vac.FIO, vac.RN_E, vac.START_DAY
+                   ) t
+             ) tt
+           GROUP BY tt.FIO, tt.RN)
+           PIVOT (MAX(HOLIDAY)
+             FOR RN
+             IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))) h
        JOIN (SELECT
+               -- джойн чтобы посчитать Итого
                fvac.FIO,
-               SUM(fvac.COUNTER) HOLIDAY
+               SUM(fvac.COUNTER) HOLIDAY -- Итого
              FROM (SELECT
                      emp.SUMMARY                     FIO,
                      vc.DATE_END - vc.DATE_START + 1 COUNTER
@@ -68,14 +87,14 @@ CREATE OR REPLACE VIEW V_REP_X-MEN_HEADERS_HOLIDAY AS
                            FROM V_VACATIONS_CHANGES v
                            WHERE v.REASON_ID = '13040') vc
                        ON emp.SUMMARY = vc.FIO
-                   WHERE vc.DATE_START > TRUNC(TO_DATE('2016-01-01', 'yyyy-mm-dd'))
-                         AND vc.DATE_END < TRUNC(TO_DATE('2016-12-31', 'yyyy-mm-dd'))
+                   WHERE vc.DATE_START > TRUNC(TO_DATE('2016-01-01', 'YYYY-MM-DD'))
+                         AND vc.DATE_END < TRUNC(TO_DATE('2016-12-31', 'YYYY-MM-DD'))
                    ORDER BY FIO) fvac
              GROUP BY fvac.FIO
             ) i ON i.FIO = h.ФИО) s
-  
-    JOIN
-  
+
+    JOIN -- обработка ситуации, когда отпуск попал на границу двух месяцев
+
     (SELECT *
      FROM
        (SELECT
@@ -94,9 +113,12 @@ CREATE OR REPLACE VIEW V_REP_X-MEN_HEADERS_HOLIDAY AS
                    vac.RN_E RN,
                    CASE vac.END_MONTH
                    WHEN vac.START_MONTH
-                     THEN NULL -- удаление даты начала, если отпуск длиной один день
-                   ELSE vac.START_DAY || '.' || vac.START_MONTH || '-' || vac.END_DAY || '.' ||
-                        vac.END_MONTH -- через чёрточку
+                     -- дата удаляется, если отпуск длиной один день
+                     THEN NULL
+                   -- если попал на границу, то вторая часть отпуска была с первого дня месяца
+                   -- и месяц начала соответственно такой же как месяц конца отпуска
+                   ELSE '01' || '.' || vac.END_MONTH || '-' || vac.END_DAY || '.' ||
+                        vac.END_MONTH
                    END      HOLIDAY
                  FROM
                    V_REP_CITEB_HEADERS_DATES vac
