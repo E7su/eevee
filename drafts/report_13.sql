1e231984564fabc568c11ec71b5de079379aa325
Etsu <etsu4296@gmail.com>
2016-08-19 12:07:36 +0300

    remove redundant view

--- drafts/report_13.sql
+++ drafts/report_13.sql
@@ -5,47 +5,33 @@
 --                    registration ( state / vendor )
 
 --------------------------------------//13//---------------------------------------
-CREATE OR REPLACE VIEW V_WORK_DURATION AS
-  SELECT
-    emp."Дата приёма",
-    NVL(emp."Дата увольнения", CURRENT_DATE) "Работа до",
-    emp."Оформление",
-    emp."Подразделение"
-  FROM V_EMPLOYEES_SHORT emp
-  WHERE emp."Оформление" IN ('Штат', 'Вентра')
-
-
--- WITH dates (d) AS (
---   SELECT TO_DATE('2014-01-01', 'yyyy-mm-dd') AS d
---   FROM dual --start
---   UNION ALL
---   SELECT ADD_MONTHS(d, 1) AS d
---   FROM dates
---   WHERE d < CURRENT_DATE /*--end*/
--- )
--- SELECT
---   dates.D,
---   s."Подразделение",
---   s."Оформление",
---   --   стаж в годах,
---   --   для  уволенных -на дату увольнения,
---   --   для  работающих -на текущую дату
---   ROUND(AVG(MONTHS_BETWEEN(s."Работа до", s."Дата приёма") / 12), 2) "Ср-я пр-ть, лет"
--- FROM dates
---   LEFT JOIN (
---               SELECT *
---               FROM V_WORK_DURATION
---             ) s
---     ON (dates.d >= s."Дата приёма" --OR s."Дата приёма" IS NULL
---         AND dates.d <= s."Работа до")
--- WHERE  s."Оформление" IS NOT NULL
---
--- -- // --
--- -- Штат
--- -- Вентра
---
--- -- УБПиВЭП
--- -- УРПИПр
--- -- УРЭК
--- -- ЦИТЭБ
--- GROUP BY dates.d, s."Подразделение", s."Оформление"
+WITH dates (d) AS (
+  SELECT TO_DATE('2014-01-01', 'yyyy-mm-dd') AS d
+  FROM dual --start
+  UNION ALL
+  SELECT ADD_MONTHS(d, 1) AS d
+  FROM dates
+  WHERE d < CURRENT_DATE /*--end*/
+)
+SELECT
+  dates.D,
+  s."Подразделение",
+  s."Оформление",
+  --   стаж в годах,
+  --   для  уволенных -на дату увольнения,
+  --   для  работающих -на текущую дату
+  ROUND(AVG(MONTHS_BETWEEN(s."Работа до", s."Дата приёма") / 12), 2) "Ср-я пр-ть, лет"
+FROM dates
+  LEFT JOIN (
+              SELECT
+                emp."Дата приёма",
+                NVL(emp."Дата увольнения", CURRENT_DATE) "Работа до",
+                emp."Оформление",
+                emp."Подразделение"
+              FROM V_EMPLOYEES_SHORT emp
+              WHERE emp."Оформление" IN ('Штат', 'Вентра')
+            ) s
+    ON (dates.d >= s."Дата приёма"
+        AND dates.d <= s."Работа до")
+WHERE s."Оформление" IS NOT NULL
+GROUP BY dates.d, s."Подразделение", s."Оформление"
